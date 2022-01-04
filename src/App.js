09b6869a92c4a27d404a611fc9a51422b0c919da
bhaveshpatil344 <bhaveshpatil344@gmail.com>
2022-01-04 15:18:26 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import "./App.css";
-
-function App() {
-  return;
-  <div>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem voluptatem
-    placeat quod corporis id fugiat non consequuntur commodi molestiae
-    perferendis vero blanditiis, exercitationem consectetur quis, tempora iusto
-    vel hic porro? Accusamus, beatae quo tenetur dolor possimus aut eligendi
-    nisi maiores eum, natus asperiores aliquid id vel modi sequi nam. Vitae
-    autem voluptate est modi adipisci odit dignissimos nesciunt quaerat, dolorum
-    distinctio commodi, non nihil animi debitis et necessitatibus. Explicabo
-    nisi sequi laudantium quae obcaecati aperiam, quod non possimus nesciunt
-    vero, cupiditate dolorem, voluptatibus tenetur. Quo eum, perspiciatis quidem
-    quia doloremque quam qui, nostrum sapiente ratione at distinctio cumque in
-    est.
-  </div>;
-  <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci
-    mollitia molestias obcaecati! In deleniti cupiditate voluptates veritatis
-    nihil tenetur similique placeat at, quidem unde repellat! Dicta deserunt ut
-    similique vero impedit animi aliquam ipsa numquam esse soluta veniam
-    repellendus saepe incidunt, sit cupiditate ratione optio non pariatur
-    adipisci ducimus tempora illum iste placeat. Accusamus quam doloribus, a
-    dolore temporibus ea perferendis expedita voluptatem quo nemo porro
-    obcaecati culpa esse neque commodi ratione autem? Quia error nisi alias
-    excepturi blanditiis velit nulla suscipit enim corporis sequi delectus
-    aperiam repellat reiciendis quisquam dolorum possimus, illum natus aliquam
-    autem minus quam quas?
-  </div>;
+export default function App() {
+  return (
+    <>
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+    </>
+  );
 }
 
-export default App;
+function MyComponent() {
+  return <div>This is my component</div>;
+}
