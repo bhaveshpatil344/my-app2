885f95e882063d67191a20a316f2571b1f558cfe
bhaveshpatil344 <bhaveshpatil344@gmail.com>
2022-01-04 15:17:09 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return;
-  <div>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem voluptatem
-    placeat quod corporis id fugiat non consequuntur commodi molestiae
-    perferendis vero blanditiis, exercitationem consectetur quis, tempora iusto
-    vel hic porro? Accusamus, beatae quo tenetur dolor possimus aut eligendi
-    nisi maiores eum, natus asperiores aliquid id vel modi sequi nam. Vitae
-    autem voluptate est modi adipisci odit dignissimos nesciunt quaerat, dolorum
-    distinctio commodi, non nihil animi debitis et necessitatibus. Explicabo
-    nisi sequi laudantium quae obcaecati aperiam, quod non possimus nesciunt
-    vero, cupiditate dolorem, voluptatibus tenetur. Quo eum, perspiciatis quidem
-    quia doloremque quam qui, nostrum sapiente ratione at distinctio cumque in
-    est.
-  </div>;
-  <div></div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
