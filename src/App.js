74ff549375f2c65cdbfdd88c904157063c996691
bhaveshpatil344 <bhaveshpatil344@gmail.com>
2022-01-04 15:50:33 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import "./App.css";
-
-function App() {
-  return;
-  <div>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem voluptatem
-    placeat quod corporis id fugiat non consequuntur commodi molestiae
-    perferendis vero blanditiis, exercitationem consectetur quis, tempora iusto
-    vel hic porro? Accusamus, beatae quo tenetur dolor possimus aut eligendi
-    nisi maiores eum, natus asperiores aliquid id vel modi sequi nam. Vitae
-    autem voluptate est modi adipisci odit dignissimos nesciunt quaerat, dolorum
-    distinctio commodi, non nihil animi debitis et necessitatibus. Explicabo
-    nisi sequi laudantium quae obcaecati aperiam, quod non possimus nesciunt
-    vero, cupiditate dolorem, voluptatibus tenetur. Quo eum, perspiciatis quidem
-    quia doloremque quam qui, nostrum sapiente ratione at distinctio cumque in
-    est.
-  </div>;
-  <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci
-    mollitia molestias obcaecati! In deleniti cupiditate voluptates veritatis
-    nihil tenetur similique placeat at, quidem unde repellat! Dicta deserunt ut
-    similique vero impedit animi aliquam ipsa numquam esse soluta veniam
-    repellendus saepe incidunt, sit cupiditate ratione optio non pariatur
-    adipisci ducimus tempora illum iste placeat. Accusamus quam doloribus, a
-    dolore temporibus ea perferendis expedita voluptatem quo nemo porro
-    obcaecati culpa esse neque commodi ratione autem? Quia error nisi alias
-    excepturi blanditiis velit nulla suscipit enim corporis sequi delectus
-    aperiam repellat reiciendis quisquam dolorum possimus, illum natus aliquam
-    autem minus quam quas?
-  </div>;
+export default function App() {
+  return (
+    <>
+      <MyComponent />
+    </>
+  );
 }
 
-export default App;
+function MyComponent() {
+  let username = "Bhavesh";
+  let email = "Bhaveshpatil344@gmail.com";
+  let id = 100;
+  let list = ["jalgaon", "calcutta", "chennai"];
+
+  // CAREFUL :: When working with Object
+  let obj = { id: 1, name: "rahul" };
+
+  return (
+    <div>
+      <h1>Leranig Interpolation {"jalgaon".toUpperCase()} </h1>
+      <h1>
+        {username} = {email} & {id}
+      </h1>
+      <h1>{list}</h1>
+      <h1>
+        {obj.id} {obj.name}
+        {`I am also JS ${obj.id} and ${obj.name}`}
+      </h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam nisi
+        autem eius libero omnis excepturi rem non consequuntur aut dolorum
+        asperiores aperiam deserunt nihil, consequatur, numquam ipsum saepe.
+        Architecto, odio.
+      </p>
+    </div>
+  );
+}
