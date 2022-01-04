bbd0034c92e369c6d036249edc57b66b0543ec7b
bhaveshpatil344 <bhaveshpatil344@gmail.com>
2022-01-04 16:21:28 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
+import { useState } from "react";
+
 export default function App() {
   return (
-    <>
-      <MyComponent />
-    </>
+    <div>
+      <MyComponent></MyComponent>
+    </div>
   );
 }
 
 function MyComponent() {
-  let username = "Bhavesh";
-  let email = "Bhaveshpatil344@gmail.com";
-  let id = 100;
-  let list = ["jalgaon", "calcutta", "chennai"];
+  let [counter, abc] = useState(100);
 
-  // CAREFUL :: When working with Object
-  let obj = { id: 1, name: "rahul" };
-
+  const increment = () => {
+    counter = counter + 1;
+    abc(counter);
+  };
   return (
     <div>
-      <h1>Leranig Interpolation {"jalgaon".toUpperCase()} </h1>
-      <h1>
-        {username} = {email} & {id}
-      </h1>
-      <h1>{list}</h1>
-      <h1>
-        {obj.id} {obj.name}
-        {`I am also JS ${obj.id} and ${obj.name}`}
-      </h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam nisi
-        autem eius libero omnis excepturi rem non consequuntur aut dolorum
-        asperiores aperiam deserunt nihil, consequatur, numquam ipsum saepe.
-        Architecto, odio.
-      </p>
+      <h1>Counter Application</h1>
+      <input type="button" value="Click Meee" onClick={increment} />
+      <div>Like {counter}</div>
     </div>
   );
 }
